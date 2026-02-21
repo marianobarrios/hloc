f6c3af21719413c5c501229ac1d27e25dafcfaa5
Mariano Barrios <git@mbarrios.net>
2026-02-21 12:33:21 +0100

    Add progress bar

--- src/stats.rs
+++ src/stats.rs
@@ -40,19 +40,14 @@ impl AddAssign for HistoricStats {
 
 impl CodeStats {
     pub fn zero() -> Self {
-        Self {
-            languages: HashMap::new(),
-        }
+        Self { languages: HashMap::new() }
     }
 
     pub fn generate(path: &Path) -> Self {
         let mut tokei_languages = tokei::Languages::new();
 
         // as we are counting on a clean clone, we don't need to ignore local files
-        let config = Config {
-            no_ignore: Some(true),
-            ..Default::default()
-        };
+        let config = Config { no_ignore: Some(true), ..Default::default() };
 
         tokei_languages.get_statistics(&[path], &[], &config);
         Self::from_tokei_stats(&tokei_languages)
@@ -66,10 +61,7 @@ impl CodeStats {
                 let line_stats = reports.iter().map(|l| l.stats.code).sum();
                 children.insert(language_type, line_stats);
             }
-            let language_stats = LanguageStats {
-                line_count: tokei_lang.code,
-                children,
-            };
+            let language_stats = LanguageStats { line_count: tokei_lang.code, children };
             languages.insert(language_type, language_stats);
         }
         Self { languages }
@@ -87,10 +79,7 @@ impl AddAssign for CodeStats {
 
 impl LanguageStats {
     pub fn zero() -> Self {
-        Self {
-            line_count: 0,
-            children: HashMap::new(),
-        }
+        Self { line_count: 0, children: HashMap::new() }
     }
 }
 
